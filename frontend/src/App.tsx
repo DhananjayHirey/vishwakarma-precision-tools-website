6e0b3811a82eb1cbd5202249205fd080874d24f4
DhananjayHirey <147147105+DhananjayHirey@users.noreply.github.com>
2025-11-14 22:46:08 +0530

    project setup with UI library

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { ScrollProgress } from "@/components/ui/scroll-progress";
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ScrollProgress />
+    </>
+  );
+}
+
+export default App;
