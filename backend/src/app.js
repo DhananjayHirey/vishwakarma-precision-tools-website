db00895d266f775f31d17f24d95c742ee927b558
DhananjayHirey <147147105+DhananjayHirey@users.noreply.github.com>
2025-12-15 14:13:42 +0530

    ...

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173" ||
-        "https://vishwakarma-precision-tools-website-phi.vercel.app/",
+      "http://localhost:5173",
+      "https://vishwakarma-precision-tools-website-phi.vercel.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
